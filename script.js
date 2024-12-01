ac66382e4bf4a96ae81cc0fdc3ebaf2bfee05a30
Галина Слесарева <galaslesareva@gmail.com>
2024-12-01 03:16:09 +0300

    add Задание урока 28

--- script.js
+++ script.js
@@ -1,62 +1,81 @@
-// Урок 28 Задание 1. Выбери тачку
+// Урок 28 Задание 3. Конвертер валют
 "use strict";
 
-// применение обработчика ошибок при подключении слушателя на несуществующий элемент
-const cars = document.querySelector('#cars');
-const result = document.querySelector('.result');
+// вводимые данные формы запроса
+const converter = document.forms.converter.elements;
+// отображаемые данные формы
+const nameIn = document.querySelector('.name-in');
+const nameTo = document.querySelector('.name-to');
+// кнопка конвертирования
+const сonvert = document.querySelector('.converter .button');
 
-// получение по AJAX запросу 
-const getData = () => fetch('./db.json')
-    .then(response => response.json())
-    .catch(() => { return { cars: [] }; });
+// курсы
+const exchangeRates = {
+    EUR: "",
+    USD: "",
+    error: true
+};
 
-// вывод результа выбора
-const outChoice = (dataCar) => {
-    if (cars.selectedIndex) {
-        let text = ``;
-        dataCar.forEach((car) => {
-            text += `Тачка ${car.brand} ${car.model}
-            Цена: ${car.price}$
-            `;
-        });
-        result.innerText = text;
-    } else {
-        result.textContent = cars[0].value;
+// отображения выбора валют
+const sayChoice = () => {
+    const currency = {
+        RUB: "Российский рубль (RUB)",
+        EUR: "Евро (EUR)",
+        USD: "Доллар США (USD)"
+    };
+    nameIn.textContent = converter['in-rub'].checked ? currency.RUB :
+        converter.usd.checked ? currency.USD : currency.EUR;
+    nameTo.textContent = converter['in-cur'].checked ? currency.RUB :
+        converter.usd.checked ? currency.USD : currency.EUR;
+};
+
+document.forms.converter.addEventListener('change', (e) => {
+    if (e.target.name === 'in-choise' || e.target.name === 'currency') {
+        sayChoice();
+        converter['curr-in-curr'].value = '';
     }
-};
-
-// формирование списка машинок
-const addCars = () => {
-    getData()
-        .then(data => {
-            data.cars.forEach(car => {
-                const option = document.createElement('option');
-                option.textContent = car.brand;
-                option.value = car.brand;
-                cars.append(option);
-            });
-            outChoice();
-        });
-};
-
-// отбор бренда машинок
-const selectBrend = (brand) => {
-    getData()
-        .then(data => {
-            const dataCar = data.cars.filter(car => car.brand.includes(brand));
-            outChoice(dataCar);
-        });
-};
-
-// выбор машинки
-cars.addEventListener('change', (e) => {
-    if (e.target.selectedIndex) {
-        selectBrend(e.target[e.target.selectedIndex].value);
-    } else {
-        outChoice();
+});
+document.forms.converter.addEventListener('input', (e) => {
+    if (e.target.name === 'input-in') {
+        e.target.value = e.target.value.replace(/\D+/gi, "");
+        converter['curr-in-curr'].value = '';
     }
+});
+document.forms.converter.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+// конвертация
+сonvert.addEventListener('click', (e) => {
+    const inCurr = converter['in-rub'].checked ? 'RUB' :
+        converter.usd.checked ? 'USD' : 'EUR';
+    const toCurr = converter['in-cur'].checked ? 'RUB' :
+        converter.usd.checked ? 'USD' : 'EUR';
+    let value = 0;
+    if (exchangeRates.error) {
+    } else if (inCurr === 'RUB') {
+        value = Math.round(+converter['input-in'].value / exchangeRates[toCurr] * 10000) / 10000;
+    } else {
+        value = Math.round(+converter['input-in'].value * exchangeRates[inCurr] * 10000) / 10000;
+    }
+    converter['curr-in-curr'].value = value ? +value.toFixed(4) : '';
 });
 
 
-addCars();
-
+fetch("https://www.cbr-xml-daily.ru/daily_json.js")
+    .then(response => response.json())
+    .then((data) => {
+        exchangeRates.EUR = data.Valute.EUR.Value;
+        exchangeRates.USD = data.Valute.USD.Value;
+        document.getElementById('comment').textContent =
+            `* по официальному курсу на ${data.Date.slice(8, 10) + data.Date.slice(4, 8) + data.Date.slice(0, 4)}`;
+        exchangeRates.error = false;
+    })
+    .catch(error => {
+        exchangeRates.EUR = '';
+        exchangeRates.USD = '';
+        document.getElementById('comment').textContent = '* курсы валют недоступны ';
+        exchangeRates.error = true;
+    });
+// текущий выбор валют    
+sayChoice();
